c28189df2ae1488f0eef21ca42a7684c8d4bc082
Nikita Skrebov <sama.niki@mail.ru>
2022-04-09 18:16:43 +0300

    Change

--- src/test/java/com/epam/tc/hw9/TestRunner.java
+++ src/test/java/com/epam/tc/hw9/TestRunner.java
@@ -1,8 +1,12 @@
 package com.epam.tc.hw9;
 
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.json.simple.JSONObject;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import lombok.SneakyThrows;
@@ -24,19 +28,6 @@ public class TestRunner {
     private String LIST_ID;
     private String CARD_ID;
 
-    public void setBOARD_ID(String BOARD_ID) {
-        this.BOARD_ID = BOARD_ID;
-    }
-
-    public void setLIST_ID(String LIST_ID) {
-        this.LIST_ID = LIST_ID;
-    }
-
-    public void setCARD_ID(String CARD_ID) {
-        this.CARD_ID = CARD_ID;
-    }
-
-
     private final String DOMAIN = "https://api.trello.com/1/";
     private final String BOARD_ENDPOINT = "boards/{id}";
     private final String LIST_ENDPOINT = "lists/{id}";
@@ -54,19 +45,7 @@ public class TestRunner {
     }
 
     @BeforeMethod
-    public void setup() {
-        REQUEST_SPECIFICATION = new RequestSpecBuilder()
-                .addHeader("Authorization", getProperties().get("login").toString())
-                .setBaseUri(DOMAIN)
-                .build();
-    }
-
-    @AfterMethod
-    void teardown() {
-    }
-
-    @Test(description = "Create a Board")
-    void createBoard() {
+    public void setUpBoardId() {
         JSONObject requestParams = new JSONObject();
         requestParams.put("name", "Board");
 
@@ -75,14 +54,26 @@ public class TestRunner {
                 .body(requestParams.toJSONString())
                 .post("boards")
                 .then()
-                .statusCode(HttpStatus.SC_OK)
                 .extract().response();
-
-        setBOARD_ID(response.jsonPath().getString("id"));
+        BOARD_ID = response.jsonPath().getString("id");
     }
 
-    @Test(description = "Create a List on Board")
-    void createBoardList() {
+    @BeforeMethod
+    public void setUpCardId() {
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("idList", LIST_ID);
+
+        Response response = given(REQUEST_SPECIFICATION)
+                .header("Content-Type","application/json")
+                .body(requestParams.toJSONString())
+                .post("cards")
+                .then()
+                .extract().response();
+        CARD_ID = response.jsonPath().getString("id");
+    }
+
+    @BeforeMethod
+    public void setUpBoardListId() {
         JSONObject requestParams = new JSONObject();
         requestParams.put("name", "List");
         requestParams.put("idBoard", BOARD_ID);
@@ -92,10 +83,50 @@ public class TestRunner {
                 .body(requestParams.toJSONString())
                 .post("lists")
                 .then()
-                .statusCode(HttpStatus.SC_OK)
                 .extract().response();
+        LIST_ID = response.jsonPath().getString("id");
+    }
 
-        setLIST_ID(response.jsonPath().getString("id"));
+
+    @BeforeClass
+    public void setup() {
+        REQUEST_SPECIFICATION = new RequestSpecBuilder()
+                .addHeader("Authorization", getProperties().get("login").toString())
+                .setBaseUri(DOMAIN)
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .build();
+    }
+
+    @AfterMethod
+    void teardown() {
+    }
+
+    @Test(description = "Create a Board")
+    public void createBoard() {
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("name", "Board");
+
+        given(REQUEST_SPECIFICATION)
+                .header("Content-Type","application/json")
+                .body(requestParams.toJSONString())
+                .post("boards")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test(description = "Create a List on Board")
+    void createList() {
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("name", "List");
+        requestParams.put("idBoard", BOARD_ID);
+
+        given(REQUEST_SPECIFICATION)
+                .header("Content-Type","application/json")
+                .body(requestParams.toJSONString())
+                .post("lists")
+                .then()
+                .statusCode(HttpStatus.SC_OK);
     }
 
     @Test(description = "Create a Card")
@@ -103,15 +134,12 @@ public class TestRunner {
         JSONObject requestParams = new JSONObject();
         requestParams.put("idList", LIST_ID);
 
-        Response response = given(REQUEST_SPECIFICATION)
+        given(REQUEST_SPECIFICATION)
                 .header("Content-Type","application/json")
                 .body(requestParams.toJSONString())
                 .post("cards")
                 .then()
-                .statusCode(HttpStatus.SC_OK)
-                .extract().response();
-
-        setCARD_ID(response.jsonPath().getString("id"));
+                .statusCode(HttpStatus.SC_OK);
     }
 
     @Test(description = "Get a Board")
@@ -146,7 +174,7 @@ public class TestRunner {
     }
 
     @Test(description = "Update a Board")
-    void giveBoardNewName() {
+    void updateBoard() {
         JSONObject requestParams = new JSONObject();
         requestParams.put("name", NEW_BOARD_NAME);
 
@@ -162,7 +190,7 @@ public class TestRunner {
     }
 
     @Test(description = "Update a List")
-    void giveListNewName() {
+    void updateList() {
         JSONObject requestParams = new JSONObject();
         requestParams.put("name", NEW_LIST_NAME);
 
@@ -177,7 +205,7 @@ public class TestRunner {
     }
 
     @Test(description = "Update a Card")
-    void giveCardNewName() {
+    void updateCard() {
         JSONObject requestParams = new JSONObject();
         requestParams.put("name", NEW_CARD_NAME);
 
@@ -191,22 +219,22 @@ public class TestRunner {
                 .body("name", equalTo(NEW_CARD_NAME));
     }
 
-    @Test(description = "Delete a Card")
-    public void killCard() {
-        given(REQUEST_SPECIFICATION)
-                .pathParam("id", CARD_ID)
-                .when()
-                .delete(CARD_ENDPOINT)
-                .then()
-                .statusCode(HttpStatus.SC_OK);
-    }
-
     @Test(description = "Delete a Board")
-    public void terminateBoard() {
+    public void deleteBoard() {
         given(REQUEST_SPECIFICATION)
                 .pathParam("id", BOARD_ID)
                 .when()
                 .delete(BOARD_ENDPOINT)
+                .then()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test(description = "Delete a Card")
+    public void deleteCard() {
+        given(REQUEST_SPECIFICATION)
+                .pathParam("id", CARD_ID)
+                .when()
+                .delete(CARD_ENDPOINT)
                 .then()
                 .statusCode(HttpStatus.SC_OK);
     }
